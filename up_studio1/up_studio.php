16a84f43c24732e905f5c19c82b73ca073a0c020
PradipRandive <90404608+PradipRandive@users.noreply.github.com>
2021-09-09 22:21:21 +0530

    Add files via upload

---
+++ up_studio1/up_studio.php
@@ -0,0 +1,142 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link rel="stylesheet" href="style.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="row srow">
+        <div class="col-md-12  text-center mt-5">
+            <img class="logo-img"  src="images/sitelogo.png" alt="">
+        </div>
+    </div>
+    <div class="row srow mt-3 p-3">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 hover p-3 ">
+            <p class="simple-text text-center">We are a disruptive digital design and development studio. Collectively,
+                 our team has designed  37 apps 
+                that are slated to launch this year. Also, we have built   63 apps   from ideation to prototype for our clients.
+                 We thrive on the experience and design thinking we learnt over years servicing clients accross the globe .</p>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+    <div class="row srow text-center ">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="row srow">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <div class="row srow">
+                    <div class="col-md-4"><img class="identity-logo" src="images/2.png" alt=""></div>
+                    <div class="col-md-4"><img class="identity-logo" src="images/3.png" alt=""></div>
+                    <div class="col-md-4"><img class="identity-logo" src="images/4.png" alt=""></div>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+    <div class="row srow mt-5">
+       <div class="col-md-3"></div>
+       <div class="col-md-6 hover p-3">
+           <h1 class="heading text-center">industries we seasoned
+            </br>with our tinch of salt!</h1>
+       </div>
+       <div class="col-md-3"></div>
+    </div>
+    <div class="row srow mt-5">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="row srow text-center">
+                <div class="col-md-3"><button type="button" class="btn neomorphism">FinTech</button></div>
+                <div class="col-md-3"><button type="button" class="btn neomorphism">HealthTech</button></div>
+                <div class="col-md-3"><button type="button" class="btn neomorphism">Blockchain</button></div>
+                <div class="col-md-3"><button type="button" class="btn neomorphism">EdTech</button></div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+    <div class="row srow mt-5">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="row text-center">
+                <div class="col-md-3"></div>
+                <div class="col-md-2"><button type="button" class="btn neomorphism">AI/ML</button></div>
+                <div class="col-md-2"><button type="button" class="btn neomorphism">B2B</button></div>
+                <div class="col-md-2"><button type="button" class="btn neomorphism">B2C</button></div>
+                <div class="col-md-3"></div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+    <div class="row srow mt-4  mb-5">
+        <div class="col-md-2"></div>
+        <div class="col-md-4 card-bg mr-2 p-3">
+            <h5>Looking for an augmented team?</h5>
+            <p class="card-text1">We are a dedicated team of enthusiasts offering </br>agility,
+                 best quality deliverance at feasible rates</br> with our multi skills. 
+            </p>
+            <img class="arrow" src="images/5.png" alt="">
+        </div>
+        <div class="col-md-4 neomorphism ml-2 p-3">
+        <h5 class="heading1">Looking for an augmented team?</h5>
+            <p class="card-text2">We are a dedicated team of enthusiasts offering </br>agility,
+                 best quality deliverance at feasible rates</br> with our multi skills. 
+            </p>
+            <img class="arrow" src="images/5.png" alt="">
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+    <div class="row srow mt-5">
+       <div class="col-md-3"></div>
+       <div class="col-md-6 hover p-3">
+           <h1 class="heading text-center">why we are worth our salt</h1>
+           <p class="card-text2 text-center">Starting from scratch, or have a headstart already? </br>
+               No matter, we have you covered! We</br> help you to build on your vision and bring it to life. 
+            </p>
+       </div>
+       <div class="col-md-3"></div>
+    </div>
+    <div class="row srow mt-5">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-2 card-bg mr-1 p-3">
+                <img class="card-img1" src="images/6.png" alt="">
+                <p class="card-text3 text-center">Understanding concept, opinion and experience 
+                </p>
+                </div>
+                <div class="col-md-2 ml-1 mr-1 neomorphism p-3">
+                <img class="card-img1" src="images/7.png" alt="">
+                <p class="card-text3 text-center">Redefining problems to find the best solutions 
+                </p>
+                </div>
+                <div class="col-md-2 ml-1 mr-1 neomorphism p-3">
+                <img class="card-img1" src="images/8.png" alt="">
+                <p class="card-text3 text-center">Understanding human behaviour and user journey
+                </p>
+                </div>
+                <div class="col-md-2 ml-1 neomorphism p-3">
+                <img class="card-img1" src="images/9.png" alt="">
+                <p class="card-text3 text-center">Lorem ipsum
+                </p>
+                </div>
+                <div class="col-md-2"></div>
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+         
+</body>
+</html>
+
